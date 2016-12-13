52ba7c3cb5aa5741e3a98a1c7dcfbafa7bdd7ad4
James-Adam Renquinha Henri <jarhmander@gmail.com>
2016-12-12 23:22:01 -0500

    Warns about no TARGET specified, and error if SRC is not specified.
    Test to support this is committed too.

--- src/make/function/makevars.mk
+++ src/make/function/makevars.mk
@@ -18,11 +18,17 @@ generate_rules_list ?=
 handled_extensions ?=
 
 define generate_rules_imp =
+    $$(if $$(strip $${TARGET}),,$$(warning TARGET not set; default to a.out)\
+		$$(eval TARGET:=a.out))
+
+    $$(if $$(strip $${SRC}),,$$(error SRC is empty; specify some source to build))
+
     $(eval ${generate_rules_list})
     unhandled_files := $$(filter-out ${handled_extensions},${SRC})
     $$(if $${unhandled_files},\
         $$(warning Unhandled files in SRC: $${unhandled_files})\
     )
+
 endef
 
 define generate_rules =
